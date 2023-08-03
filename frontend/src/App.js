c6e2d89b7e7174cac48a15457f2cfc7263fe5b67
Akshat Jalan <Jalanakshat2@gmail.com>
2023-08-03 06:01:42 +0530

    update frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,33 @@
-import "./App.css";
+import "../src/App.css";
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
+import React from "react";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
 
 function App() {
-  return <div className="App">welcome</div>;
+  const Layout = () => {
+    return (
+      <div className="app">
+        <Navbar />
+        <Outlet />
+      </div>
+    );
+  };
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+      ],
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
 }
 
 export default App;
