ef258e48185e79d4c0bb12ffa607e74dad4c3676
Akshat Jalan <Jalanakshat2@gmail.com>
2023-08-13 02:12:07 +0530

    Navbar layout completed

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -18,13 +18,24 @@ function Navbar() {
       <div className="container">
         <div className="logo">
           <Link className="link" to="/">
-            <span className="text">Code FreeLance</span>
+            <span>CodeLance</span>
           </Link>
         </div>
         <div className="links">
-          <span className="username">Explore</span>
+          <Link className="nav_links" to="/">
+            Find Developers
+          </Link>
+          <Link className="nav_links" to="/">
+            Hire Coders
+          </Link>
+          <Link className="nav_links" to="/">
+            Showcase Skills
+          </Link>
+          <Link className="nav_links" to="/">
+            Discover Gigs
+          </Link>
           {!currentUser?.isSeller && (
-            <button className="navbarButtons">Become a Seller</button>
+            <button className="navbarButtons">Become a Developer</button>
           )}
           {currentUser ? (
             <div className="user" onClick={() => setOpen(!open)}>
@@ -32,7 +43,7 @@ function Navbar() {
                 src="https://images.pexels.com/photos/1115697/pexels-photo-1115697.jpeg?auto=compress&cs=tinysrgb&w=1600"
                 alt=""
               />
-              <span className="username">{currentUser?.username}</span>
+              <span className="nav_links">{currentUser?.username}</span>
               {open && (
                 <div className="options">
                   {currentUser.isSeller && (
@@ -59,48 +70,14 @@ function Navbar() {
             </div>
           ) : (
             <>
-              <span>Sign in</span>
+              {/* <Link className="nav_links">Sign in</Link> */}
               <Link className="link" to="/register">
-                <button className="navbarButtons">Join</button>
+                <button className="joinButton">Sign Up</button>
               </Link>
             </>
           )}
         </div>
       </div>
-
-      <>
-        <hr />
-        <div className="menu">
-          <Link className="link menuLink" to="/">
-            Debugging
-          </Link>
-          <Link className="link menuLink" to="/">
-            Web Development
-          </Link>
-          <Link className="link menuLink" to="/">
-            App Development
-          </Link>
-          <Link className="link menuLink" to="/">
-            Code Review
-          </Link>
-          <Link className="link menuLink" to="/">
-            Testing
-          </Link>
-          <Link className="link menuLink" to="/">
-            Design UI/UX
-          </Link>
-          <Link className="link menuLink" to="/">
-            AI Services
-          </Link>
-
-          <Link className="link menuLink" to="/">
-            Programming & Tech
-          </Link>
-          <Link className="link menuLink" to="/">
-            Business
-          </Link>
-        </div>
-      </>
     </div>
   );
 }
