8ca01ad86aa699e4bae484aa779896bf86aa8de2
Akshat Jalan <Jalanakshat2@gmail.com>
2023-08-25 04:02:46 +0530

    Added messages and message page

--- frontend/src/pages/Message.js
+++ frontend/src/pages/Message.js
@@ -1,7 +1,134 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import "../styles/Message.css";
 
-function Message() {
-  return <div>Message</div>;
-}
+const Message = () => {
+  return (
+    <div className="message">
+      <div className="messageContainer">
+        <span className="breadcrumbs">
+          <Link to="/messages">Messages</Link>➤ Akshat Jalan ➤
+        </span>
+        <div className="chat">
+          <div className="chatItem">
+            <img
+              src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
+              alt=""
+            />
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos iure
+              mollitia perspiciatis officiis voluptate? Sequi quae officia
+              possimus, iusto labore alias mollitia eveniet nemo placeat
+              laboriosam nisi animi! Error, tenetur!
+            </p>
+          </div>
+          <div className="owner">
+            <img
+              src="https://images.pexels.com/photos/1115697/pexels-photo-1115697.jpeg?auto=compress&cs=tinysrgb&w=1600"
+              alt=""
+            />
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos iure
+              mollitia perspiciatis officiis voluptate? Sequi quae officia
+              possimus, iusto labore alias mollitia eveniet nemo placeat
+              laboriosam nisi animi! Error, tenetur!
+            </p>
+          </div>
+          <div className="chatItem">
+            <img
+              src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
+              alt=""
+            />
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos iure
+              mollitia perspiciatis officiis voluptate? Sequi quae officia
+              possimus, iusto labore alias mollitia eveniet nemo placeat
+              laboriosam nisi animi! Error, tenetur!
+            </p>
+          </div>
+          <div className="owner">
+            <img
+              src="https://images.pexels.com/photos/1115697/pexels-photo-1115697.jpeg?auto=compress&cs=tinysrgb&w=1600"
+              alt=""
+            />
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos iure
+              mollitia perspiciatis officiis voluptate? Sequi quae officia
+              possimus, iusto labore alias mollitia eveniet nemo placeat
+              laboriosam nisi animi! Error, tenetur!
+            </p>
+          </div>
+          <div className="owner">
+            <img
+              src="https://images.pexels.com/photos/1115697/pexels-photo-1115697.jpeg?auto=compress&cs=tinysrgb&w=1600"
+              alt=""
+            />
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos iure
+              mollitia perspiciatis officiis voluptate? Sequi quae officia
+              possimus, iusto labore alias mollitia eveniet nemo placeat
+              laboriosam nisi animi! Error, tenetur!
+            </p>
+          </div>
+          <div className="chatItem">
+            <img
+              src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
+              alt=""
+            />
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos iure
+              mollitia perspiciatis officiis voluptate? Sequi quae officia
+              possimus, iusto labore alias mollitia eveniet nemo placeat
+              laboriosam nisi animi! Error, tenetur!
+            </p>
+          </div>
+          <div className="owner">
+            <img
+              src="https://images.pexels.com/photos/1115697/pexels-photo-1115697.jpeg?auto=compress&cs=tinysrgb&w=1600"
+              alt=""
+            />
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos iure
+              mollitia perspiciatis officiis voluptate? Sequi quae officia
+              possimus, iusto labore alias mollitia eveniet nemo placeat
+              laboriosam nisi animi! Error, tenetur!
+            </p>
+          </div>
+
+          <div className="owner">
+            <img
+              src="https://images.pexels.com/photos/1115697/pexels-photo-1115697.jpeg?auto=compress&cs=tinysrgb&w=1600"
+              alt=""
+            />
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos iure
+              mollitia perspiciatis officiis voluptate? Sequi quae officia
+              possimus, iusto labore alias mollitia eveniet nemo placeat
+              laboriosam nisi animi! Error, tenetur!
+            </p>
+          </div>
+
+          <div className="chatItem">
+            <img
+              src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
+              alt=""
+            />
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos iure
+              mollitia perspiciatis officiis voluptate? Sequi quae officia
+              possimus, iusto labore alias mollitia eveniet nemo placeat
+              laboriosam nisi animi! Error, tenetur!
+            </p>
+          </div>
+        </div>
+        <hr />
+        <div className="write">
+          <textarea type="text" placeholder="write a message" />
+          <button className="Buttons">Send</button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Message;
