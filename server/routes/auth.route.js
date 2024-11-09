7e85f05492f56d07d826adf8fd323c75c5243759
Akshat Jalan <Jalanakshat2@gmail.com>
2024-11-09 19:03:43 +0530

    added models

--- server/routes/auth.route.js
+++ server/routes/auth.route.js
@@ -3,8 +3,8 @@ import { register, login, logout } from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
-router.post("/register", register)
-router.post("/login", login)
-router.post("/logout", logout)
+router.post("/register", register);
+router.post("/login", login);
+router.post("/logout", logout);
 
 export default router;
--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -4,7 +4,7 @@ import { verifyToken } from "../middleware/jwt.js";
 
 const router = express.Router();
 
-router.delete("/:id", verifyToken, deleteUser);
+router.delete("/:id", verifyToken, deleteUser); //verifyToken will call jwt middleware to check token is valid, than delete function will called
 router.get("/:id", getUser);
 
 export default router;
