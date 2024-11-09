4a19be6075ff9c1666f70e0648adf431802f4fb5
Akshat Jalan <Jalanakshat2@gmail.com>
2024-11-10 00:56:52 +0530

    added controllers

---
+++ server/controllers/auth.controller.js
@@ -0,0 +1,59 @@
+import User from "../models/user.model.js";
+import createError from "../utils/createError.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+export const register = async (req, res, next) => {
+  try {
+    const hash = bcrypt.hashSync(req.body.password, 5); //used hash library to encrypt password
+    const newUser = new User({
+      ...req.body,
+      password: hash,
+    }); //passing whole body(username, email etc.. and encrypted password)
+
+    await newUser.save();
+    res.status(201).send("User has been created.");
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const login = async (req, res, next) => {
+  try {
+    const user = await User.findOne({ username: req.body.username }); //using mongodb findone function to find user
+
+    if (!user) return next(createError(404, "User not found!"));
+
+    const isCorrect = bcrypt.compareSync(req.body.password, user.password);
+    if (!isCorrect)
+      return next(createError(400, "Incorrect username or password!"));
+
+    const token = jwt.sign(
+      {
+        id: user._id,
+        isSeller: user.isSeller,
+      },
+      process.env.JWT_KEY
+    );
+
+    const { password, ...info } = user._doc; //user contains a lot of extra data .doc contains the actual user data
+    res
+      .cookie("accessToken", token, {
+        httpOnly: true,
+      })
+      .status(200)
+      .send(info);
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const logout = async (req, res) => {
+  res
+    .clearCookie("accessToken", {
+      sameSite: "none",
+      secure: true,
+    })
+    .status(200)
+    .send("User has been logged out.");
+};
--- server/server.js
+++ server/server.js
@@ -1,6 +1,14 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRoute from "./routes/user.route.js";
+// import gigRoute from "./routes/gig.route.js";
+// import orderRoute from "./routes/order.route.js";
+// import conversationRoute from "./routes/conversation.route.js";
+// import messageRoute from "./routes/message.route.js";
+// import reviewRoute from "./routes/review.route.js";
+import authRoute from "./routes/auth.route.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
 dotenv.config();
@@ -14,7 +22,27 @@ const connectDB = async () => {
   }
 };
 
+//middlewares
+app.use(express.json());
+app.use(cookieParser());
+
+//Api routes
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+// app.use("/api/gigs", gigRoute);
+// app.use("/api/orders", orderRoute);
+// app.use("/api/conversations", conversationRoute);
+// app.use("/api/messages", messageRoute);
+// app.use("/api/reviews", reviewRoute);
+
+app.use((err, req, res, next) => {
+  const errorStatus = err.status || 500;
+  const errorMessage = err.message || "Something went wrong!";
+
+  return res.status(errorStatus).send(errorMessage);
+});
+
 app.listen(8800, () => {
   connectDB();
-  console.log("connected");
+  console.log("Started");
 });
