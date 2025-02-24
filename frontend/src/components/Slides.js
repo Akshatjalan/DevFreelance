3f78fd17d3bae184615ec1b7aa22dc7417482ade
Akshat Jalan <Jalanakshat2@gmail.com>
2025-02-25 05:13:30 +0530

    build for netlify deployment

--- frontend/src/components/Slides.js
+++ frontend/src/components/Slides.js
@@ -1,14 +1,15 @@
 import React from "react";
 import "../styles/Slides.css";
-import Slider from "infinite-react-carousel";
+// import Slider from "infinite-react-carousel";
 
-const Slides = ({ children, slidesToShow, arrowsScroll }) => {
+const Slides = () => {
   return (
     <div className="slide">
       <div className="slideContainer">
-        <Slider slidesToShow={slidesToShow} arrowsScroll={arrowsScroll}>
+        {/* <Slider slidesToShow={slidesToShow} arrowsScroll={arrowsScroll}>
           {children}
-        </Slider>
+        </Slider> */}
+        Slider
       </div>
     </div>
   );
